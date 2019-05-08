7f0861bf8b503410918ee072639f7971e31a8a61
Sergey Lyubka <support@cesanta.com>
2019-05-09 00:44:57 +0100

    release 1.0.11

--- src/mDash.h
+++ src/mDash.h
@@ -11,16 +11,19 @@ extern "C" {
   mDashInitWithWifi((a), (b), (c), (d), __FILE__)
 #define mDashStart(a, b) mDashInit((a), (b), __FILE__)
 
+enum { MDASH_EV_CONNECTED, MDASH_EV_DISCONNECTED };
+
 // mDash housekeeping
 void mDashInitWithWifi(const char *wifi_name, const char *wifi_pass,
                        const char *device_id, const char *device_pass,
                        const char *app_name);
 void mDashInit(const char *id, const char *pass, const char *app_name);
+void mDashOn(void (*fn)(int, void *), void *);
 void mDashSetLogLevel(int logLevel);  // 0 - no logs, 3 - debug logs
 const char *mDashGetDeviceID(void);
 
 // MQTT API
-bool mDashPublish(const char *topic, const char *message);
+bool mDashPublish(const char *topic, const char *message_fmt, ...);
 void mDashSubscribe(const char *topic, void (*fn)(const char *, const char *));
 
 // RPC API
@@ -33,7 +36,7 @@ const char *mDashGetParams(void *ctx);
 int mDashGetNum(const char *json, const char *json_path, double *value);
 int mDashGetStr(const char *json, const char *json_path, char *dst, int len);
 int mDashGetBase64(const char *json, const char *json_path, char *dst, int len);
-int mDashGetBool(const char *json, const char *json_path, char *dst, int len);
+int mDashGetBool(const char *json, const char *json_path, int *);
 
 #ifdef __cplusplus
 }
