5bf9d245e6547d689bead96bf9eb2103d4d5d904
Sergey Lyubka <support@cesanta.com>
2019-05-12 17:57:57 +0100

    release 1.0.13

--- src/mDash.h
+++ src/mDash.h
@@ -8,22 +8,22 @@ extern "C" {
 #endif
 
 #define mDashStartWithWifi(a, b, c, d) \
-  mDashInitWithWifi((a), (b), (c), (d), __FILE__)
-#define mDashStart(a, b) mDashInit((a), (b), __FILE__)
+  mDashInitWithWifi((a), (b), (c), (d), __FILE__, __DATE__ "-" __TIME__)
+#define mDashStart(a, b) mDashInit((a), (b), __FILE__, __DATE__ "-" __TIME__)
 
 enum { MDASH_EV_CONNECTED, MDASH_EV_DISCONNECTED };
 
 // mDash housekeeping
 void mDashInitWithWifi(const char *wifi_name, const char *wifi_pass,
                        const char *device_id, const char *device_pass,
-                       const char *app_name);
-void mDashInit(const char *id, const char *pass, const char *app_name);
+                       const char *app_name, const char *build_time);
+void mDashInit(const char *, const char *, const char *, const char *);
 void mDashOn(void (*fn)(int, void *), void *);
 void mDashSetLogLevel(int logLevel);
 const char *mDashGetDeviceID(void);
 
 // MQTT API
-bool mDashPublish(const char *topic, const char *message_fmt, ...);
+int mDashPublish(const char *topic, const char *message_fmt, ...);
 void mDashSubscribe(const char *topic, void (*fn)(const char *, const char *));
 
 // RPC API
