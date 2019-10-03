ceaba3bc1ac912b653e686773eb63f20e0ec9ee6
Sergey Lyubka <lsm@cesanta.com>
2019-10-03 09:28:32 +0100

    release 1.1.7

---
+++ examples/GpioReadWrite/GpioReadWrite.ino
@@ -0,0 +1,57 @@
+// GPIO read/write via the RPC mechanism.
+// Read more on RPC at https://mdash.net/docs/userguide/remote_control.md
+// JSON-RPC API is documented at: https://github.com/cesanta/mjson
+//
+// - Install mDash library:
+//   - Select "Sketch" &rarr; "Include Library" &rarr; "Manage Libraries"
+//   - In the search field, type "mDash" and press Enter
+//   - Click on "Install" to install the library
+// - Select "Tools" → "Board" → "ESP32 Dev Module"
+// - Select "Tools" → "Partitioning Scheme" → "Minimal SPIFFS"
+// - Select "Tools" → "Port" → your serial port
+// - Click on "Upload" button to build and flash the firmware
+
+#define MDASH_APP_NAME "GpioControlApp"
+#include <mDash.h>
+
+#include <WiFi.h>
+
+#define WIFI_NETWORK "MyWifiNetworkName"
+#define WIFI_PASSWORD "MyWifiPassword"
+#define DEVICE_PASSWORD "mDashDeviceToken"
+
+static void handleGpioRead(struct jsonrpc_request *r) {
+  double pin;
+  if (mjson_get_number(r->params, r->params_len, "$.pin", &pin)) {
+    pinMode(pin, OUTPUT);
+    jsonrpc_return_success(r, "%d", digitalRead(pin));
+  } else {
+    jsonrpc_return_error(r, JSONRPC_ERROR_BAD_PARAMS, "%Q", "pin required");
+  }
+}
+
+static void handleGpioWrite(struct jsonrpc_request *r) {
+  double pin, value;
+  if (mjson_get_number(r->params, r->params_len, "$.pin", &pin) &&
+      mjson_get_number(r->params, r->params_len, "$.value", &value)) {
+    pinMode(pin, INPUT);
+    digitalWrite(pin, value);
+    jsonrpc_return_success(r, "true");
+  } else {
+    jsonrpc_return_error(r, JSONRPC_ERROR_BAD_PARAMS, "%Q",
+                         "pin & value required");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+  mDashBegin(DEVICE_PASSWORD);
+  jsonrpc_export("GPIO.Read", handleGpioRead, NULL);
+  jsonrpc_export("GPIO.Write", handleGpioWrite, NULL);
+}
+
+void loop() {
+  delay(3000);
+  mDashNotify("DB.Save", "{%Q:%lu}", "ram", mDashGetFreeRam());  // Report data
+}
--- src/mDash.h
+++ src/mDash.h
@@ -45,15 +45,17 @@ void mDashInit(const char *device_id, const char *device_pass,
 
 void mDashSetLogLevel(int logLevel);
 void mDashSetServer(const char *, int);
+int mDashGetState(void);  // current connection state, MDASH_EVENT_*
 const char *mDashGetSdkVersion(void);
 unsigned long mDashGetFreeRam(void);
+char *mDashToJson(const char *json_fmt, ...);
 
 // Events
 enum {
   // Events defined by the mDash library
   MDASH_EVENT_NETWORK_LOST = 0,       // event_data: NULL
   MDASH_EVENT_NETWORK_AP = 1,         // event_data: NULL
-  MDASH_EVENT_NETWORK_CONNECTED = 2,  // event_data: struct in_addr *
+  MDASH_EVENT_NETWORK_CONNECTED = 2,  // event_data: NULL
   MDASH_EVENT_CLOUD_CONNECTED = 3,    // event_data: NULL
 
   // Events defined by user
@@ -76,20 +78,6 @@ int mDashNotify(const char *name, const char *fmt, ...);
 #define mDashShadowUpdate(fmt, ...) \
   mDashNotify("Dash.Shadow.Update", (fmt), __VA_ARGS__)
 
-// RPC API
-void mDashExport(const char *name, void (*cb)(void *, void *), void *cbdata);
-void mDashReturnSuccess(void *ctx, const char *json_fmt, ...);
-void mDashReturnError(void *ctx, const char *error_message);
-const char *mDashGetParams(void *ctx);
-
-// JSON API
-int mDashGetNum(const char *json, const char *json_path, double *value);
-int mDashGetStr(const char *json, const char *json_path, char *dst, int len);
-int mDashGetBase64(const char *json, const char *json_path, char *dst, int len);
-int mDashGetBool(const char *json, const char *json_path, int *);
-int mDashGetTok(const char *json, const char *json_path, const char **, int *);
-char *mDashToJson(const char *json_fmt, ...);
-
 // Configuration API
 int mDashConfigGet(const char *name, char *buf, int bufsize);
 int mDashConfigSet(const char *name, const char *value);
@@ -99,12 +87,164 @@ void mDashCLI(unsigned char input_byte);
 // JS API
 int mDashInitJS(int ram_size);
 
-// mjson API
+// mjson API - see documentation at https://github.com/cesanta/mjson
+#ifndef MJSON_H
+#define MJSON_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef MJSON_ENABLE_PRINT
+#define MJSON_ENABLE_PRINT 1
+#endif
+
+#ifndef MJSON_ENABLE_RPC
+#define MJSON_ENABLE_RPC 1
+#endif
+
+#ifndef MJSON_ENABLE_BASE64
+#define MJSON_ENABLE_BASE64 1
+#endif
+
+#ifndef MJSON_RPC_IN_BUF_SIZE
+#define MJSON_RPC_IN_BUF_SIZE 256
+#endif
+
+#ifndef ATTR
+#define ATTR
+#endif
+
+enum {
+  MJSON_ERROR_INVALID_INPUT = -1,
+  MJSON_ERROR_TOO_DEEP = -2,
+};
+
+enum mjson_tok {
+  MJSON_TOK_INVALID = 0,
+  MJSON_TOK_KEY = 1,
+  MJSON_TOK_STRING = 11,
+  MJSON_TOK_NUMBER = 12,
+  MJSON_TOK_TRUE = 13,
+  MJSON_TOK_FALSE = 14,
+  MJSON_TOK_NULL = 15,
+  MJSON_TOK_ARRAY = 91,
+  MJSON_TOK_OBJECT = 123,
+};
+#define MJSON_TOK_IS_VALUE(t) ((t) > 10 && (t) < 20)
+
+typedef void (*mjson_cb_t)(int ev, const char *s, int off, int len, void *ud);
+
+#ifndef MJSON_MAX_DEPTH
+#define MJSON_MAX_DEPTH 20
+#endif
+
+int mjson(const char *s, int len, mjson_cb_t cb, void *ud);
+enum mjson_tok mjson_find(const char *s, int len, const char *jp,
+                          const char **tokptr, int *toklen);
+int mjson_get_number(const char *s, int len, const char *path, double *v);
+int mjson_get_bool(const char *s, int len, const char *path, int *v);
+int mjson_get_string(const char *s, int len, const char *path, char *to, int n);
+
+#if MJSON_ENABLE_BASE64
+int mjson_get_base64(const char *s, int len, const char *path, char *to, int n);
+#endif
+
+#if MJSON_ENABLE_PRINT
 typedef int (*mjson_print_fn_t)(const char *buf, int len, void *userdata);
 typedef int (*mjson_vprint_fn_t)(mjson_print_fn_t, void *, va_list *);
-int mjson_printf(mjson_print_fn_t, void *, const char *, ...);
+
+struct mjson_fixedbuf {
+  char *ptr;
+  int size, len;
+};
+
+int mjson_printf(mjson_print_fn_t, void *, const char *fmt, ...);
+int mjson_vprintf(mjson_print_fn_t, void *, const char *fmt, va_list ap);
+int mjson_print_str(mjson_print_fn_t, void *, const char *s, int len);
+int mjson_print_int(mjson_print_fn_t, void *, int value, int is_signed);
+int mjson_print_long(mjson_print_fn_t, void *, long value, int is_signed);
+
 int mjson_print_file(const char *ptr, int len, void *userdata);
+int mjson_print_fixed_buf(const char *ptr, int len, void *userdata);
 int mjson_print_dynamic_buf(const char *ptr, int len, void *userdata);
+
+#endif  // MJSON_ENABLE_PRINT
+
+#if MJSON_ENABLE_RPC
+
+void jsonrpc_init(void (*response_cb)(const char *, int, void *),
+                  void *userdata);
+
+struct jsonrpc_request {
+  const char *params;   // Points to the "params" in the request frame
+  int params_len;       // Length of the "params"
+  const char *id;       // Points to the "id" in the request frame
+  int id_len;           // Length of the "id"
+  mjson_print_fn_t fn;  // Printer function
+  void *fndata;         // Printer function data
+  void *userdata;       // Callback's user data as specified at export time
+};
+
+struct jsonrpc_method {
+  const char *method;
+  int method_sz;
+  void (*cb)(struct jsonrpc_request *);
+  void *cbdata;
+  struct jsonrpc_method *next;
+};
+
+// Main RPC context, stores current request information and a list of
+// exported RPC methods.
+struct jsonrpc_ctx {
+  struct jsonrpc_method *methods;
+  void *userdata;
+  void (*response_cb)(const char *buf, int len, void *userdata);
+  int in_len;
+  char in[MJSON_RPC_IN_BUF_SIZE];
+};
+
+// Registers function fn under the given name within the given RPC context
+#define jsonrpc_ctx_export(ctx, name, fn, ud)                                \
+  do {                                                                       \
+    static struct jsonrpc_method m = {(name), sizeof(name) - 1, (fn), 0, 0}; \
+    m.cbdata = (ud);                                                         \
+    m.next = (ctx)->methods;                                                 \
+    (ctx)->methods = &m;                                                     \
+  } while (0)
+
+void jsonrpc_ctx_init(struct jsonrpc_ctx *ctx,
+                      void (*response_cb)(const char *, int, void *),
+                      void *userdata);
+int jsonrpc_call(mjson_print_fn_t fn, void *fndata, const char *fmt, ...);
+void jsonrpc_return_error(struct jsonrpc_request *r, int code,
+                          const char *message_fmt, ...);
+void jsonrpc_return_success(struct jsonrpc_request *r, const char *result_fmt,
+                            ...);
+void jsonrpc_ctx_process(struct jsonrpc_ctx *ctx, char *req, int req_sz,
+                         mjson_print_fn_t fn, void *fndata);
+void jsonrpc_ctx_process_byte(struct jsonrpc_ctx *ctx, unsigned char ch,
+                              mjson_print_fn_t fn, void *fndata);
+
+extern struct jsonrpc_ctx jsonrpc_default_context;
+
+#define jsonrpc_export(name, fn, ud) \
+  jsonrpc_ctx_export(&jsonrpc_default_context, (name), (fn), (ud))
+
+#define jsonrpc_process(buf, len, fn, data) \
+  jsonrpc_ctx_process(&jsonrpc_default_context, (buf), (len), (fn), (data))
+
+#define jsonrpc_process_byte(x, fn, data) \
+  jsonrpc_ctx_process_byte(&jsonrpc_default_context, (x), (fn), (data))
+
+#define JSONRPC_ERROR_INVALID -32700    /* Invalid JSON was received */
+#define JSONRPC_ERROR_NOT_FOUND -32601  /* The method does not exist */
+#define JSONRPC_ERROR_BAD_PARAMS -32602 /* Invalid params passed */
+#define JSONRPC_ERROR_INTERNAL -32603   /* Internal JSON-RPC error */
+
+#endif  // MJSON_ENABLE_RPC
+#endif  // MJSON_H
 
 #ifdef __cplusplus
 }
